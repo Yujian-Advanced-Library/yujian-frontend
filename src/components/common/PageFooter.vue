5cf5f105c5474c1c840fc2edbc432fcc203b773f
derecknowayback <dereck_yak@foxmail.com>
2024-10-20 11:35:09 +0800

    feat(components): 添加关于我们的页面组件和通用的页头页脚组件
    - 新增 AboutUs.vue组件，包含关于我们页面的布局和样式
    - 新增 PageFooter.vue 和 PageHeader.vue 组件，作为通用的页头和页脚组件
    - 三个组件都采用了 Vue 3 的 <script setup>语法糖
    - 页面样式使用了 Tailwind CSS 进行快速布局和样式调整

---
+++ src/components/common/PageFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="bg-white p-10 flex justify-evenly">
+    <ul class="list-none m-0 p-0 flex">
+      <li class="mr-2">
+        <a class="block font-bold text-black text-center px-4 py-2" href="#AboutUs">关于我们</a>
+      </li>
+      <li class="mr-2">
+        <a class="block font-bold text-black text-center px-4 py-2" href="#ContactUs">联系我们</a>
+      </li>
+      <li class="mr-2">
+        <a class="block font-bold text-black text-center px-4 py-2" href="#HelpCenter">帮助中心</a>
+      </li>
+      <li class="mr-2">
+        <a class="block font-bold text-black text-center px-4 py-2" href="#PrivacyPolicy">隐私条款</a>
+      </li>
+      <li class="mr-2">
+        <a class="block font-bold text-black text-center px-4 py-2" href="#UserAgreement">用户协议</a>
+      </li>
+      <li>
+        <a class="block font-bold text-black text-center px-4 py-2" href="#MerchantCooperation">商家合作</a>
+      </li>
+    </ul>
+  </footer>
+</template>
